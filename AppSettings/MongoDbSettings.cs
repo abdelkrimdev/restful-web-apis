4aa268afa098fc28f4ddd1b919150506312d5ed8
Abdelkrim Dib <abdelkrim-dev@outlook.com>
2017-08-28 19:49:45 +0100

    configure mongodb

---
+++ AppSettings/MongoDbSettings.cs
@@ -0,0 +1,11 @@
+namespace SupaTrupa.WebAPI.AppSettings
+{
+    public class MongoDbSettings
+    {
+        public string User { get; set; }
+        public string Pass { get; set; }
+        public string Host { get; set; }
+        public string Port { get; set; }
+        public string Data { get; set; }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -4,18 +4,22 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
+using SupaTrupa.WebAPI.AppSettings;
+using SupaTrupa.WebAPI.Shared.Contracts;
+using SupaTrupa.WebAPI.Shared.MongoDb;
+
 namespace SupaTrupa.WebAPI
 {
     public class Startup
     {
         public Startup(IHostingEnvironment env)
         {
-            var builder = new ConfigurationBuilder()
+            Configuration = new ConfigurationBuilder()
                 .SetBasePath(env.ContentRootPath)
                 .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
                 .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
-                .AddEnvironmentVariables();
-            Configuration = builder.Build();
+                .AddEnvironmentVariables()
+                .Build();
         }
 
         public IConfigurationRoot Configuration { get; }
@@ -24,18 +28,31 @@ namespace SupaTrupa.WebAPI
         public void ConfigureServices(IServiceCollection services)
         {
             // Add framework services.
-            services.AddMvc();
+            services
+                .Configure<MongoDbSettings>(
+                    (options) =>
+                    {
+                        options.User = Configuration.GetValue<string>("MongoDb:User");
+                        options.Pass = Configuration.GetValue<string>("MongoDb:Pass");
+                        options.Host = Configuration.GetValue<string>("MongoDb:Host");
+                        options.Port = Configuration.GetValue<string>("MongoDb:Port");
+                        options.Data = Configuration.GetValue<string>("MongoDb:Data");
+                    }
+                )
+                .AddCors()
+                .AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
-            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
-            loggerFactory.AddDebug();
+            loggerFactory
+                .AddConsole(Configuration.GetSection("Logging"))
+                .AddDebug();
 
             app
                 .UseCors(
-                    (options) => 
+                    (options) =>
                         options
                             .AllowAnyOrigin()
                             .AllowAnyHeader()
