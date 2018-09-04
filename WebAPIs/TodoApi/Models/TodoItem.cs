71de9bd2e2ed5c3ff92de3fb34256bdeaaff1931
Abdelkrim Dib <abdelkrim-dev@outlook.com>
2018-09-04 22:13:50 +0100

    implement the TodoItem model

---
+++ WebAPIs/TodoApi/Models/TodoItem.cs
@@ -0,0 +1,12 @@
+﻿using Shared.Abstracts;
+using Shared.Attributes;
+
+namespace TodoApi.Models
+{
+    [CollectionName("TodoItem")]
+    public class TodoItem : Entity
+    {
+        public string Name { get; set; }
+        public bool IsComplete { get; set; }
+    }
+}
