1b744269f454af2d67301ca9f92a3ea254b63151
Abdelkrim Dib <abdelkrim-dev@outlook.com>
2018-09-06 19:50:32 +0100

    create a test project for the TodoApi

---
+++ WebAPIs/TodoApi.Tests/Controllers/TodoItemsControllerTests.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+using System.Threading.Tasks;
+
+namespace TodoApi.Tests.Controllers
+{
+    [TestFixture]
+    public class TodoItemsControllerTests
+    {
+        [SetUp]
+        public void Initialize()
+        {
+        }
+
+        [TearDown]
+        public void DropDataBase()
+        {
+        }
+
+        [Test]
+        public async Task GetAsyncTest()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+        }
+    }
+}
