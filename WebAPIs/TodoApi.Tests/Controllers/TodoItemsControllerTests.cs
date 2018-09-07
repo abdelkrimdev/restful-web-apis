4e76de118117bed8d9abfea4b92621b74ddc1346
Abdelkrim Dib <abdelkrim-dev@outlook.com>
2018-09-07 22:52:38 +0100

    test the Get endpoint

--- WebAPIs/TodoApi.Tests/Controllers/TodoItemsControllerTests.cs
+++ WebAPIs/TodoApi.Tests/Controllers/TodoItemsControllerTests.cs
@@ -1,29 +1,32 @@
-﻿using NUnit.Framework;
+﻿using GenFu;
+using NSubstitute;
+using NUnit.Framework;
+using Shared.Contracts;
+using System.Collections.Generic;
 using System.Threading.Tasks;
+using TodoApi.Controllers;
+using TodoApi.Models;
 
 namespace TodoApi.Tests.Controllers
 {
     [TestFixture]
     public class TodoItemsControllerTests
     {
-        [SetUp]
-        public void Initialize()
-        {
-        }
-
-        [TearDown]
-        public void DropDataBase()
-        {
-        }
-
         [Test]
-        public async Task GetAsyncTest()
+        public async Task GetTest()
         {
             // Arrange
+            int pageNumber = 1, pageSize = 50;
+            var todoItemsRepository = Substitute.For<IRepository<TodoItem>>();
+            todoItemsRepository.GetAsync(pageNumber, pageSize)
+                               .Returns(Task.Run<IEnumerable<TodoItem>>(() => A.ListOf<TodoItem>(20)));
+            var controller = new TodoItemsController(todoItemsRepository);
 
             // Act
+            var todos = await controller.Get();
 
             // Assert
+            Assert.IsNotNull(todos);
         }
     }
 }
