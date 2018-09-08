d97d304fda69fc8c7280a2aee3e387f70e496783
Abdelkrim Dib <abdelkrim-dev@outlook.com>
2018-09-08 13:04:22 +0100

    implement a test for get todo by id

--- WebAPIs/TodoApi.Tests/Controllers/TodoItemsControllerTests.cs
+++ WebAPIs/TodoApi.Tests/Controllers/TodoItemsControllerTests.cs
@@ -1,4 +1,5 @@
 ﻿using GenFu;
+using Microsoft.AspNetCore.Mvc;
 using NSubstitute;
 using NUnit.Framework;
 using Shared.Contracts;
@@ -13,20 +14,48 @@ namespace TodoApi.Tests.Controllers
     public class TodoItemsControllerTests
     {
         [Test]
-        public async Task GetTest()
+        public async Task GetAsync_ShouldReturnPaginatedListOfTodoItems()
         {
             // Arrange
             int pageNumber = 1, pageSize = 50;
+            var todos = A.ListOf<TodoItem>(pageSize);
             var todoItemsRepository = Substitute.For<IRepository<TodoItem>>();
-            todoItemsRepository.GetAsync(pageNumber, pageSize)
-                               .Returns(Task.Run<IEnumerable<TodoItem>>(() => A.ListOf<TodoItem>(20)));
+            todoItemsRepository.GetAsync(pageNumber, pageSize).Returns(todos);
             var controller = new TodoItemsController(todoItemsRepository);
 
             // Act
-            var todos = await controller.Get();
+            var response = await controller.Get(pageNumber, pageSize);
+            var result = response.Result as ObjectResult;
+            var value = result.Value as IEnumerable<TodoItem>;
+            var statusCode = result.StatusCode;
 
             // Assert
-            Assert.IsNotNull(todos);
+            Assert.IsNotNull(value);
+            Assert.IsNotNull(statusCode);
+            Assert.AreEqual(value, todos);
+            Assert.AreEqual(statusCode, 200);
+        }
+
+        [Test]
+        public async Task GetAsync_ShouldReturnSingleTodoItemById()
+        {
+            // Arrange
+            var todo = A.New<TodoItem>();
+            var todoItemsRepository = Substitute.For<IRepository<TodoItem>>();
+            todoItemsRepository.GetAsync(todo.Id).Returns(todo);
+            var controller = new TodoItemsController(todoItemsRepository);
+
+            // Act
+            var response = await controller.Get(todo.Id);
+            var result = response.Result as ObjectResult;
+            var value = result.Value as TodoItem;
+            var statusCode = result.StatusCode;
+
+            // Assert
+            Assert.IsNotNull(value);
+            Assert.IsNotNull(statusCode);
+            Assert.AreEqual(value, todo);
+            Assert.AreEqual(statusCode, 200);
         }
     }
 }
