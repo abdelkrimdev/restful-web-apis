b229315af9c70b83c8415adec8dd521aaa1eb24f
Abdelkrim Dib <abdelkrim-dev@outlook.com>
2018-09-08 16:23:20 +0100

    create test for get todo not found

--- WebAPIs/TodoApi.Tests/Controllers/TodoItemsControllerTests.cs
+++ WebAPIs/TodoApi.Tests/Controllers/TodoItemsControllerTests.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using NSubstitute;
 using NUnit.Framework;
 using Shared.Contracts;
+using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using TodoApi.Controllers;
@@ -26,14 +27,34 @@ namespace TodoApi.Tests.Controllers
             // Act
             var response = await controller.Get(pageNumber, pageSize);
             var result = response.Result as ObjectResult;
-            var value = result.Value as IEnumerable<TodoItem>;
+            var value = result.Value as List<TodoItem>;
             var statusCode = result.StatusCode;
 
             // Assert
             Assert.IsNotNull(value);
             Assert.IsNotNull(statusCode);
-            Assert.AreEqual(value, todos);
+            Assert.AreEqual(value.Count, todos.Count);
             Assert.AreEqual(statusCode, 200);
+        }
+
+        [Test]
+        public async Task GetAsync_ShouldReturnNotFoundWhenTodoItemIsMissing()
+        {
+            // Arrange
+            TodoItem todo = null;
+            var id = Guid.NewGuid().ToString();
+            var todoItemsRepository = Substitute.For<IRepository<TodoItem>>();
+            todoItemsRepository.GetAsync(id).Returns(todo);
+            var controller = new TodoItemsController(todoItemsRepository);
+
+            // Act
+            var response = await controller.Get(id);
+            var result = response.Result as NotFoundResult;
+            var statusCode = result.StatusCode;
+
+            // Assert
+            Assert.IsNotNull(statusCode);
+            Assert.AreEqual(statusCode, 404);
         }
 
         [Test]
@@ -54,7 +75,7 @@ namespace TodoApi.Tests.Controllers
             // Assert
             Assert.IsNotNull(value);
             Assert.IsNotNull(statusCode);
-            Assert.AreEqual(value, todo);
+            Assert.AreEqual(value.Id, todo.Id);
             Assert.AreEqual(statusCode, 200);
         }
     }
