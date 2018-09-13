ffe9e601fe810b7f129852797d493998e095b769
Abdelkrim Dib <abdelkrim-dev@outlook.com>
2018-09-13 22:28:56 +0100

    put create user script into scripts folder

---
+++ WebAPIs/scripts/createuser.sh
@@ -0,0 +1,3 @@
+﻿mongo \
+    --host localhost --port 27017 -u root_username -p very_secure_root_pass --authenticationDatabase admin \
+    --eval "db.createUser({user: 'iodine', pwd: 'secret', roles:[{role:'dbOwner', db: 'supaTrupa'}]});"
