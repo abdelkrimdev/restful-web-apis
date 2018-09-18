bf53cb68caff516ea564cb2acdb1afb737e0be89
Abdelkrim Dib <abdelkrim-dev@outlook.com>
2018-09-18 21:50:35 +0100

    create a JSON utility class

---
+++ WebAPIs/Shared.Tests/Utilities/JSONUtils.cs
@@ -0,0 +1,12 @@
+﻿using Newtonsoft.Json;
+
+namespace Shared.Tests.Utilities
+{
+    public static class  JSONUtils
+    {
+        public static T Deserialize<T>(string json)
+        {
+            return JsonConvert.DeserializeObject<T>(json);
+        }
+    }
+}
