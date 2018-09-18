e67e3adbeaac703451608e9784893cff03f5a37f
Abdelkrim Dib <abdelkrim-dev@outlook.com>
2018-09-18 21:49:17 +0100

    create a utility class for managing files

---
+++ WebAPIs/Shared.Tests/Utilities/FileUtils.cs
@@ -0,0 +1,12 @@
+﻿using System.IO;
+
+namespace Shared.Tests.Utilities
+{
+    public static class FileUtils
+    {
+        public static string Read(string path)
+        {
+            return File.ReadAllText(path);
+        }
+    }
+}
